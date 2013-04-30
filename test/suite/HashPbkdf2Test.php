3d5523142bee3b2acadda19da97d70b9e74f738b
Erin Millard <ezzatron@gmail.com>
2013-04-30 17:39:53 +1000

    Improved test suite.

--- test/suite/HashPbkdf2Test.php
+++ test/suite/HashPbkdf2Test.php
@@ -17,13 +17,11 @@ class HashPbkdf2Test extends PHPUnit_Framework_TestCase
     public function pbkdf2Data()
     {
         return array(
-            array('password',                 'salt',                                 1,        20, '0c60c80f961f0e71f3a9b524af6012062fe037a6'),
-            array('password',                 'salt',                                 2,        20, 'ea6c014dc72d6f8ccd1ed92ace1d41f0d8de8957'),
-            array('password',                 'salt',                                 4096,     20, '4b007901b765489abead49d926f721d065a429c1'),
-            // disabled because this test takes a large amount of time to complete
-            // array('password',                 'salt',                                 16777216, 20, 'eefe3d61cd4da4e4e9945b3d6ba2158c2634e984'),
-            array('passwordPASSWORDpassword', 'saltSALTsaltSALTsaltSALTsaltSALTsalt', 4096,     25, '3d2eec4fe41c849b80c8d83662c0e44a8b291a964cf2f07038'),
-            array("pass\0word",               "sa\0lt",                               4096,     16, '56fa6aa75548099dcc37d7f03425e0c3'),
+            array('password',                 'salt',                                 1,    20, '0c60c80f961f0e71f3a9b524af6012062fe037a6'),
+            array('password',                 'salt',                                 2,    20, 'ea6c014dc72d6f8ccd1ed92ace1d41f0d8de8957'),
+            array('password',                 'salt',                                 4096, 20, '4b007901b765489abead49d926f721d065a429c1'),
+            array('passwordPASSWORDpassword', 'saltSALTsaltSALTsaltSALTsaltSALTsalt', 4096, 25, '3d2eec4fe41c849b80c8d83662c0e44a8b291a964cf2f07038'),
+            array("pass\0word",               "sa\0lt",                               4096, 16, '56fa6aa75548099dcc37d7f03425e0c3'),
         );
     }
 
@@ -37,5 +35,15 @@ class HashPbkdf2Test extends PHPUnit_Framework_TestCase
 
         $this->assertSame($expectedHexHash, $hexHash);
         $this->assertSame(pack('H*', $expectedHexHash), $rawHash);
+    }
+
+    /**
+     * See [RFC 6070 section 2](http://tools.ietf.org/html/rfc6070#section-2).
+     *
+     * @group intensive
+     */
+    public function testHashPbkdf2WithManyIterations()
+    {
+        $this->testHashPbkdf2('password', 'salt', 16777216, 20, 'eefe3d61cd4da4e4e9945b3d6ba2158c2634e984');
     }
 }
